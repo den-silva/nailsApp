79e256276035360edb80fee95be339ed2f119018
den-silva <dmurilosilva@hotmail.com>
2024-02-24 23:18:29 -0300

    Criação da tela de login

--- nailsApp/App.js
+++ nailsApp/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';  
+import LoginClient from './src/components/Client/LoginClient'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <LoginClient />      
       <StatusBar style="auto" />
     </View>
+
+
   );
 }
 
